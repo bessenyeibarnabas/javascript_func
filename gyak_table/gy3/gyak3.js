dce52bcabf0342c713bd9393a28a0b0d06622926
bessenyeibarnabas <bessenyei.barnabas_2022c@bolyai-szakkozep.hu.noreply.github.com>
2025-10-19 11:49:47 +0200

    gyakorlas_kommentekkel

--- gyak_table/gy3/gyak3.js
+++ gyak_table/gy3/gyak3.js
@@ -1,8 +1,8 @@
 
 /**
- * @type {[szerzo: string, mu: string, mufaj: string, mu2: string, mufaj2: string]}
+* @type {{szerzo: string, mu: string, mufaj: string, mu2?: string, mufaj2?: string}[]}
  */
-const arr = [
+const arr = [ //table adatai kivéve a fejlécé
     {
         szerzo: "Petőfi Sándor",
         mu: "A helység kalapácsa",
@@ -24,54 +24,54 @@ const arr = [
     },
 ]
 
-const ht = ["Szerző","Mű","Műfaj"]
+const ht = ["Szerző","Mű","Műfaj"] //fejléc adatai
 
-const table = document.createElement("table")
+const table = document.createElement("table")  //táblázat létrehozása a html body-ban
 document.body.appendChild(table)
 
-const thead = document.createElement("thead")
+const thead = document.createElement("thead") //thead csinál hozzáfűz table
 table.appendChild(thead)
 
-const tr = document.createElement("tr")
+const tr = document.createElement("tr") //fejléc sor létrehozása thead-hez
 thead.appendChild(tr)
 
-for( const ti of ht){
+for( const ti of ht){ //fejléc adatai bejárása
     const th = document.createElement("th")
-    tr.appendChild(th)
-    th.innerText = ti
+    tr.appendChild(th) //th csinál hozzárak tr-hez
+    th.innerText = ti //th-nak tartalmat adunk
 }
 
 const tbody = document.createElement("tbody")
-table.appendChild(tbody)
+table.appendChild(tbody) //tbody létrehoz table-ben
 
-for(const i of arr){
+for(const i of arr){ //arr tömb bejárása
     const tr = document.createElement("tr")
-    tbody.appendChild(tr)
+    tbody.appendChild(tr) //tr a tbody-hoz
 
-    const td1 = document.createElement("td")
+    const td1 = document.createElement("td") //td1 tr-hez, majd feltöltés adattal
     tr.appendChild(td1)
     td1.innerText = i.szerzo
 
-    const td2 = document.createElement("td")
+    const td2 = document.createElement("td") //td2 tr-hez, majd feltöltés adattal
     tr.appendChild(td2)
     td2.innerText = i.mu
 
-    const td3 = document.createElement("td")
+    const td3 = document.createElement("td") //td3 tr-hez, majd feltöltés adattal
     tr.appendChild(td3)
     td3.innerText = i.mufaj
 
-    if(i.mu2){
-        td1.rowSpan = "2"
+    if(i.mu2){ //ha obj-ban van mu2
+        td1.rowSpan = "2" //két sor összevon
 
         const tr = document.createElement("tr")
-        tbody.appendChild(tr)
+        tbody.appendChild(tr) //tr a tbody-hoz (sor) 2. adatok miatt
 
         const td4 = document.createElement("td")
         tr.appendChild(td4)
-        td4.innerText = i.mu2
+        td4.innerText = i.mu2 //td4 tr-hez, majd feltöltés adattal
 
         const td5 = document.createElement("td")
         tr.appendChild(td5)
-        td5.innerText = i.mufaj2
+        td5.innerText = i.mufaj2 //td5 tr-hez, majd feltöltés adattal
     }
 }
--- gyak_table/gy4/gyak4.js
+++ gyak_table/gy4/gyak4.js
@@ -1,5 +1,7 @@
-
-const arr = [
+/**
+ * @type {{terulet: string, idoszak: string, esemeny: string, esemeny2?: string}[]} 
+ */
+const arr = [ //adatok fejlécen kívül
     {
         terulet: "Nyugat- és Dél-Európa",
         idoszak: "1494-1559",
@@ -18,51 +20,52 @@ const arr = [
     },
 ]
 
-const table = document.createElement("table")
+const table = document.createElement("table") //table létrehoz a html body-ban
 document.body.append(table)
 
-const thead = document.createElement("thead")
+const thead = document.createElement("thead") //thead csinál hozzáfűz table
 table.appendChild(thead)
 
-const tr = document.createElement("tr")
+const tr = document.createElement("tr") // fejléc sor létrehozása
 thead.appendChild(tr)
 
-const th1 = document.createElement("th")
+const th1 = document.createElement("th") //th csinál hozzárak tr-hez + adat (fejléc cellák létrehozása + adat)
 tr.appendChild(th1)
 th1.innerText = "Terület"
 
-const th2 = document.createElement("th")
+const th2 = document.createElement("th") //th csinál hozzárak tr-hez + adat
 tr.appendChild(th2)
 th2.innerText = "Időszak"
 
-const th3 = document.createElement("th")
+const th3 = document.createElement("th") //th csinál hozzárak tr-hez + adat
 tr.appendChild(th3)
 th3.innerText = "Esemény"
 
 const tbody = document.createElement("tbody")
-table.appendChild(tbody)
+table.appendChild(tbody)  //tbody hozzáadása a táblázathoz
 
-for(const i of arr){
+for(const i of arr){ //arr tömb bejárása
     const tr = document.createElement("tr")
-    tbody.appendChild(tr)
+    tbody.appendChild(tr) //tr a tbody-hoz (új sor)
 
+    //cellák
     const td1 = document.createElement("td")
     tr.appendChild(td1)
-    td1.innerText = i.terulet
+    td1.innerText = i.terulet //td1 tr-hez, majd feltöltés adattal
 
     const td2 = document.createElement("td")
     tr.appendChild(td2)
-    td2.innerText = i.idoszak
+    td2.innerText = i.idoszak //td2 tr-hez, majd feltöltés adattal
 
     const td3 = document.createElement("td")
     tr.appendChild(td3)
-    td3.innerText = i.esemeny
+    td3.innerText = i.esemeny //td3 tr-hez, majd feltöltés adattal
 
-    if(i.esemeny2){
+    if(i.esemeny2){ //ha obj-ban van esemyeny2
         const td4 = document.createElement("td")
         tr.appendChild(td4)
-        td4.innerText = i.esemeny2
+        td4.innerText = i.esemeny2 //td4 tr-hez, majd feltöltés adattal
     }else{
-        td3.colSpan = "2"
+        td3.colSpan = "2" //2db oszlop összevon
     }
 }

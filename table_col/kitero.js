7c32a1c1ddf49ec761fa5c3769591116c6bcf422
bessenyeibarnabas <bessenyei.barnabas_2022c@bolyai-szakkozep.hu.noreply.github.com>
2025-10-19 08:36:57 +0200

    orai

--- table_col/kitero.js
+++ table_col/kitero.js
@@ -63,7 +63,7 @@ const arr = [
 ]
 
 
-const table = document.createElement("table")
+/*const table = document.createElement("table")
 document.body.appendChild(table)
 
 
@@ -117,14 +117,56 @@ for (const elem of arr) {
         tr.appendChild(td4)
         td4.innerText = elem.lover2
     }
-}
+}*/
 
+ 
+const table = document.createElement("table");
+document.body.appendChild(table);
+ 
+const thead = document.createElement("thead");
+const tr = document.createElement("tr");
+ 
+table.appendChild(thead);
+thead.appendChild(tr);
+ 
+createCell("th", "Szerzők", tr);
+createCell("th", "Korszak", tr);
+const sz_d = createCell("th", "Szerelmek", tr)
+ 
+sz_d.colSpan = 2;
+ 
+const tbody = document.createElement("tbody");
+ 
+table.appendChild(tbody)
+ 
+for(let i = 0; i < arr.length; i++){
+    const tr = document.createElement("tr");
+ 
+    createCell("td", arr[i].author, tr)
+    createCell("td", arr[i].era, tr)
+    const sz_d2 = createCell("td", arr[i].lover1, tr)
+ 
+    if(!arr[i].lover2){
+        sz_d2.colSpan = 2;
+    }
+    else{
+        createCell("td", arr[i].lover2, tr)
+    }
+ 
+    tbody.appendChild(tr);
+}
+ 
 /**
- * 
- * @param {string} celltype //td vagy th mert válltozik
- * @param {string} cellcontent -beállítjuk a cellcontent-et
- * @param {HTMLTableRowElement} parento -hozzáfűzűnk egy sort
+ *
+ * @param {string} cellType Lehet "th" vagy "td"
+ * @param {string} cellContent A cella tartalma
+ * @param {HTMLTableRowElement} parentRow A sor amihez hozzáadunk
+ *
+ * @returns {HTMLTableCellElement} //Visszatérünk a létrehozott cella elementtel, hogy később tudjuk módosítani a tulajdonságait
  */
-function createcell(celltype,cellcontent,parento){
-    cella
+function createCell(cellType, cellContent, parentRow){
+    const cella = document.createElement(cellType);
+    cella.innerText = cellContent;
+    parentRow.appendChild(cella);
+    return cella;
 }

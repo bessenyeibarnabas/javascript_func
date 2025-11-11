148f7cc36d3d8d027d18c67cdc5bfab4c8d186d0
Bessenyei Barnabas Tamas <bessenyei.barnabas@BOLYAI.LOCAL>
2025-11-11 11:25:17 +0100

    regebbi_hf

--- table_col/kitero.js
+++ table_col/kitero.js
@@ -81,11 +81,11 @@ thead.appendChild(tr);
 createCell("th", "Szerzők", tr);
 createCell("th", "Korszak", tr);
 const sz_d = createCell("th", "Szerelmek", tr)
- 
+
 sz_d.colSpan = 2;
  
 const tbody = document.createElement("tbody");
- 
+
 table.appendChild(tbody)
  
 for(let i = 0; i < arr.length; i++){
@@ -93,18 +93,18 @@ for(let i = 0; i < arr.length; i++){
  
     createCell("td", arr[i].author, tr)
     createCell("td", arr[i].era, tr)
-    const sz_d2 = createCell("td", arr[i].lover1, tr)
+    const sz_d2 = createCell("td", arr[i].love1, tr)
  
-    if(!arr[i].lover2){
+    if(!arr[i].love2){
         sz_d2.colSpan = 2;
     }
     else{
-        createCell("td", arr[i].lover2, tr)
+        createCell("td", arr[i].love2, tr)
     }
  
     tbody.appendChild(tr);
 }
- 
+
 /**
  *
  * @param {string} cellType Lehet "th" vagy "td"
@@ -119,3 +119,48 @@ function createCell(cellType, cellContent, parentRow){
     parentRow.appendChild(cella);
     return cella;
 }
+
+/**
+ * @param {HTMLFormElement} form
+ * @param {string} l_text
+ * @param {string} id
+ */
+function createInput(form, l_text, id) {
+    const label = document.createElement("label") //input
+    label.htmlFor = id //input / label id-je
+    label.innerText = l_text //lable-nek a szövege
+
+    form.appendChild(label) //labelt hozzárkjuk a form hoz.
+    form.appendChild(document.createElement("br")) //+sor
+
+    const input = document.createElement("input")
+    input.type = "text" //szöveg típus
+    input.id = id //inpu id-je
+    form.appendChild(input)
+    form.appendChild(document.createElement("br")) ///+sor
+    form.appendChild(document.createElement("br")) ///+sor
+}
+
+//form
+const form = document.createElement("form")
+form.id = "form_js"
+
+//h2
+const h2 = document.createElement("h2")
+h2.innerText = "javascript form "
+form.appendChild(h2)
+
+//mezők létrehoz
+createInput(form, "Költő neve:", "kolto_nev")
+createInput(form, "Korszak:", "korszak")
+createInput(form, "Szerelme:", "szerelem1")
+createInput(form, "Szerelme:", "szerelem2")
+
+//gomb hozzáfűz
+const button = document.createElement("button")
+button.type = "button"
+button.innerText = "hozzáadás"
+form.appendChild(button)
+
+//dock.body hoz fűzzük
+document.body.appendChild(form)

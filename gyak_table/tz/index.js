41443e07264457830e2dab605dd7e95a989f2fd2
bessenyeibarnabas <bessenyei.barnabas_2022c@bolyai-szakkozep.hu.noreply.github.com>
2025-11-29 18:13:39 +0100

    gyak

--- gyak_table/tz/index.js
+++ gyak_table/tz/index.js
@@ -1,3 +1,6 @@
+/**
+ * @type {{telepules: string, agazat: string, pelda: string, agazat2?: string, pelda2?: string}[]}
+ */
 const arr = [
     {
         telepules: "Athén",
@@ -40,6 +43,7 @@ for (const i of fejlec){
 }
 
 const tbody = document.createElement("tbody")
+tbody.id = "jstbody" //jstbody is
 table.appendChild(tbody)
 
 for (const item of arr){
@@ -77,7 +81,7 @@ for (const item of arr){
 const checkbox = document.getElementById("tableselector")
 const htmlDiv = document.getElementById("htmlsection")
 
-checkbox.addEventListener("change", () =>{
+function CheckboxChange(){
     if (checkbox.checked) {
         htmlDiv.classList.add("hide")
         jsDiv.classList.remove("hide")
@@ -85,4 +89,129 @@ checkbox.addEventListener("change", () =>{
         jsDiv.classList.add("hide")
         htmlDiv.classList.remove("hide")
     }
+}
+
+checkbox.addEventListener("change", CheckboxChange)
+CheckboxChange();
+
+
+//js form
+
+function generateForm (parent, txt, id, inputname){
+    const div = document.createElement("div")
+    parent.appendChild(div)
+
+    const label = document.createElement("label")
+    div.appendChild(label)
+    label.htmlFor = id
+    label.innerText = txt
+
+    generateBr(div)
+
+    const input = document.createElement("input")
+    div.appendChild(input)
+    input.type = "text"
+    input.id = id
+    input.name = inputname
+
+    generateBr(div)
+
+    const span = document.createElement("span")
+    div.appendChild(span)
+    span.classList.add("hide")
+
+    generateBr(div)
+
+}
+
+//sortörés
+function generateBr (parent){
+    const br = document.createElement("br")
+    parent.appendChild(br)
+}
+
+//adatok
+const form= document.createElement("form")
+jsDiv.appendChild(form)
+form.id="jsform" //jsform id
+
+generateForm(form,"Ókori település","elso","telepules")
+generateForm(form,"Ágazat1","masodik","agazat1")
+generateForm(form,"Példa1","harmadik","pelda1")
+generateForm(form,"Ágazat2","negyedik","agazat2")
+generateForm(form,"Példa2","otodik","pelda2")
+
+//button
+const submit= document.createElement("input")
+form.appendChild(submit)
+submit.type="submit"
+submit.value="Hozzáad_js"
+
+
+//html table-hez add adatot
+const htmlform = document.getElementById("htmlform")
+
+htmlform.addEventListener("submit", function (e){
+    e.preventDefault();
+
+    const form = e.target
+
+    //input id lekérés
+    const telepules = form.querySelector("#elso")
+    const agazat = form.querySelector("#masodik")
+    const pelda = form.querySelector("#harmadik")
+    const pelda2 = form.querySelector("#negyedik")
+
+    let valid = true;
+    const fields = [telepules, agazat, pelda];
+
+    for (const input of fields) {
+        const div = input.parentElement;
+        const error = div.querySelector(".error");
+        if (input.value === "") {
+            error.innerText = "A mező kitöltése kötelező";
+            valid = false;
+        } else {
+            error.innerText = "";
+        }
+    }
+
+    if (!valid) return;
+
+    //obj érték
+    const obj = {
+        telepules: telepules.value,
+        agazat: agazat.value,
+        pelda: pelda.value,
+        pelda2: pelda2.value === "" ? undefined:pelda2.value,
+    }
+
+    const tbody = document.getElementById("htmlbody")
+
+    const tr = document.createElement("tr")
+
+    const td1 = document.createElement("td")
+    td1.innerText = obj.telepules
+    tr.appendChild(td1)
+
+    const td2 = document.createElement("td")
+    td2.innerText = obj.agazat
+    tr.appendChild(td2)
+
+    const td3 = document.createElement("td")
+    td3.innerText = obj.pelda
+    tr.appendChild(td3)
+
+    if (obj.pelda2 == undefined){
+        td3.colSpan = 2
+    }else{
+        const td4 = document.createElement("td")
+        td4.innerText = obj.pelda2
+        tr.appendChild(td4)
+    }
+
+    tbody.appendChild(tr)
+    form.reset();
 })
+
+//js validáció
